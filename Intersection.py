be475aa2a11064c0ca45dc4c5d7736478781e090
Matthew Gordon <148798938+MatthewGordon2@users.noreply.github.com>
2023-12-11 16:07:34 +0000

    Update Intersection.py

--- Intersection.py
+++ Intersection.py
@@ -1,27 +1,22 @@
-puzzle =[[0,2,1,0,8,0,4,0,9],
-         [9,0,0,0,0,0,0,6,2],
-         [6,0,0,0,0,0,0,0,0],
-         [0,7,4,0,0,0,9,1,3],
-         [1,5,9,3,4,0,6,0,0],
-         [0,3,6,1,9,0,0,0,4],
-         [0,1,8,0,0,0,0,4,5],
-         [4,9,0,8,0,1,7,0,6],
-         [0,6,2,4,0,3,0,9,0]]
-
 import copy
+
+
+# INITIAL STEPS CODE
 
 def convert_to_listed(puzzle):
     for row in puzzle:
         for cols in range(9):
             if row[cols] == 0:
-                row[cols] = list(range(1,10))
-                
+                row[cols] = list(range(1, 10))
+
     puzzle_reduced = list_to_int(puzzle)
-    
+
     return puzzle_reduced
+
 
 def is_list(x):
     return type(x) == list
+
 
 def list_to_int(puzzle):
     for row in puzzle:
@@ -30,40 +25,45 @@ def list_to_int(puzzle):
                 row[cols] = row[cols][0]
     return puzzle
 
+# INITIAL STEPS CODE END
+
+
+# SIMPLE ELIM CODE
+
 # remove by row function
 # rowcols is to look for list of potentials, rowcols2 is the numbers already given/solved in the sudoku to remove from
 # list of potentials
 def row_remover(puzzle):
     for row in puzzle:
         for cols in range(9):
-            #if it is a list then...
+            # if it is a list then...
             if is_list(row[cols]):
-                #go through 0-8
+                # go through 0-8
                 for cols2 in range(9):
-                    #[row, col2] is not a list and [row,col2] is in the list [row,col]...
+                    # [row, col2] is not a list and [row,col2] is in the list [row,col]...
                     if not is_list(row[cols2]) and row[cols2] in row[cols]:
-                        #remove the element from the list
+                        # remove the element from the list
                         row[cols].remove(row[cols2])
-                        
+
     puzzle_reduced = list_to_int(puzzle)
-    
+
     return puzzle_reduced
+
 
 def column_remover(puzzle):
     for row in puzzle:
         for cols in range(9):
-            #if it is a list then...
+            # if it is a list then...
             if is_list(row[cols]):
-                #hold that list and look through the rest of the column
+                # hold that list and look through the rest of the column
                 for row2 in range(9):
-                    #if the element in the column is not a list and the element in the column appears in the list,
-                    #then remove it from the list.
+                    # if the element in the column is not a list and the element in the column appears in the list,
+                    # then remove it from the list.
                     if not is_list(puzzle[row2][cols]) and puzzle[row2][cols] in row[cols]:
                         row[cols].remove(puzzle[row2][cols])
-                        
-    
+
     puzzle_reduced = list_to_int(puzzle)
-    
+
     return puzzle_reduced
 
 
@@ -74,155 +74,162 @@ def box_remover(puzzle):
     for row in range(9):
         for cols in range(9):
             if is_list(puzzle[row][cols]):
-                #if its a list
+                # if its a list
                 rcols = (cols // 3) * 3
                 rrow = (row // 3) * 3
-                #seperate into 3 groups, col 1 goes to 0 col 2 goes to 0 col 9 goes to 2 etc
-                #same for rows
-                #times by 3, so 0 goes to 0, 1 goes to 3, 2 goes to 6
-                for n in range(rcols, rcols+3):
-                    #0,3
-                    #3,6
-                    #6,9
-                    for m in range(rrow, rrow+3):
+                # seperate into 3 groups, col 1 goes to 0 col 2 goes to 0 col 9 goes to 2 etc
+                # same for rows
+                # times by 3, so 0 goes to 0, 1 goes to 3, 2 goes to 6
+                for n in range(rcols, rcols + 3):
+                    # 0,3
+                    # 3,6
+                    # 6,9
+                    for m in range(rrow, rrow + 3):
                         if not is_list(puzzle[m][n]) and puzzle[m][n] in puzzle[row][cols]:
                             puzzle[row][cols].remove(puzzle[m][n])
-                            
-                            
+
     puzzle_reduced = list_to_int(puzzle)
-    
-    
+
     return puzzle_reduced
 
-def initial_step(puzzle):
-    
-    convert_to_listed(puzzle)
-    
-    same = False
-    
-    while same == False:
-        
-        puzzle_copy = copy.deepcopy(puzzle)
-    
-        row_remover(puzzle)
-        column_remover(puzzle)
-        box_remover(puzzle)
-        
-        if puzzle_copy == puzzle:
-            same = True
-            
+def simple_elim(puzzle):
+    row_remover(puzzle)
+    list_to_int(puzzle)
+    column_remover(puzzle)
+    list_to_int(puzzle)
+    box_remover(puzzle)
+    list_to_int(puzzle)
+    print('Simple_Elimination')
     return puzzle
 
-def pointing_rows(puzzle, row, col, num):
+# SIMPLE ELIM CODE END
 
+# INTERSECTION CODE
+
+def pointing_rows(puzzle2, row, col, num):
+    
+    # INDEXING
     box_col = (col // 3) * 3
     box_row = (row // 3) * 3
-    
+
     index = []
     for i in range(9):
         index.append(i)
-    
+
     index.remove(box_col)
-    index.remove(box_col+1)
-    index.remove(box_col+2)
+    index.remove(box_col + 1)
+    index.remove(box_col + 2)
     
-    #how many times num appears in lists inside that box
+    # BOX MUST BE REDUCED FOR IT TO WORK
+    # ROW MUST BE REDUCED FOR IT TO WORK
+    puzzle1 = box_remover(puzzle2)
+    puzzle = row_remover(puzzle1)
+
+    # how many times num appears in lists inside that box
     box_count = 0
-    
+
+    #for the box we are in
     for i in range(box_row, box_row + 3):
         for j in range(box_col, box_col + 3):
+            #if it is a list and the number we are looking for is in that list
             if is_list(puzzle[i][j]) and num in puzzle[i][j]:
-                box_count +=1
+                #add 1 to box count
+                box_count += 1
 
-    #how many times num appears in lists in that box in that row
+    # how many times num appears in lists in that box in that row
     row_box_count = 0
-    
+
+    #for the box we are in
     for i in range(box_col, box_col + 3):
         if is_list(puzzle[row][i]) and num in puzzle[row][i]:
             row_box_count += 1
-            
-    
+
     if box_count == row_box_count and box_count != 0:
         for i in index:
             if is_list(puzzle[row][i]) and num in puzzle[row][i]:
                 puzzle[row][i].remove(num)
 
-
     return puzzle
 
-def pointing_cols(puzzle, row, col, num):
 
+def pointing_cols(puzzle2, row, col, num):
     box_col = (col // 3) * 3
     box_row = (row // 3) * 3
-    
+
     index = []
     for i in range(9):
         index.append(i)
-    
+
     index.remove(box_row)
-    index.remove(box_row+1)
-    index.remove(box_row+2)
+    index.remove(box_row + 1)
+    index.remove(box_row + 2)
     
-    #how many times num appears in lists inside that box
+    # BOX MUST BE REDUCED FOR IT TO WORK
+    # COL MUST BE REDUCED FOR IT TO WORK
+    puzzle1 = box_remover(puzzle2)
+    puzzle = row_remover(puzzle1)
+
+    # how many times num appears in lists inside that box
     box_count = 0
-    
+
     for i in range(box_row, box_row + 3):
         for j in range(box_col, box_col + 3):
             if is_list(puzzle[i][j]) and num in puzzle[i][j]:
-                box_count +=1
+                box_count += 1
 
-    #how many times num appears in lists in that box in that column
+    # how many times num appears in lists in that box in that column
     col_box_count = 0
-    
+
     for i in range(box_row, box_row + 3):
         if is_list(puzzle[i][col]) and num in puzzle[i][col]:
             col_box_count += 1
-            
-    
+
     if box_count == col_box_count and box_count != 0:
         for i in index:
             if is_list(puzzle[i][col]) and num in puzzle[i][col]:
                 puzzle[i][col].remove(num)
 
-
     return puzzle
 
+
 def pointing_rows_reduction(puzzle):
-    
-    puzzle_reduced = initial_step(puzzle)
+    puzzle_reduced = box_remover(puzzle)
 
     for row in range(9):
         for col in range(9):
-            for num in range(1,10):
+            for num in range(1, 10):
                 puzzle_reduced2 = pointing_rows(puzzle_reduced, row, col, num)
-                
+
     puzzle_reduced3 = list_to_int(puzzle_reduced2)
-                
+
     return puzzle_reduced3
+
 
 def pointing_cols_reduction(puzzle):
-    
-    puzzle_reduced = initial_step(puzzle)
+    puzzle_reduced = box_remover(puzzle)
 
     for row in range(9):
         for col in range(9):
-            for num in range(1,10):
+            for num in range(1, 10):
                 puzzle_reduced2 = pointing_cols(puzzle_reduced, row, col, num)
-                
+
     puzzle_reduced3 = list_to_int(puzzle_reduced2)
-                
+
     return puzzle_reduced3
+
 
 def pointing_reduction(puzzle):
-    
     puzzle_reduced1 = pointing_rows_reduction(puzzle)
     puzzle_reduced2 = pointing_cols_reduction(puzzle_reduced1)
-    
+
     puzzle_reduced3 = list_to_int(puzzle_reduced2)
-    
+    print('Pointing_Reduction')
+
     return puzzle_reduced3
 
-def row_pots_list(num, row, cols, sudoku):
+
+def row_pots_list(num, row, cols, sudoku1):
+    sudoku = row_remover(sudoku1)
     row_count = 0
     for i in range(9):
         if is_list(sudoku[row][i]) and num in sudoku[row][i]:
@@ -242,7 +249,9 @@ def row_pots_list(num, row, cols, sudoku):
                     sudoku[m][n].remove(num)
     return sudoku
 
-def col_pots_list(num, row, cols, sudoku):
+
+def col_pots_list(num, row, cols, sudoku1):
+    sudoku = column_remover(sudoku1)
     col_count = 0
     for i in range(9):
         if is_list(sudoku[i][cols]) and num in sudoku[i][cols]:
@@ -262,49 +271,182 @@ def col_pots_list(num, row, cols, sudoku):
                     sudoku[n][m].remove(num)
     return sudoku
 
+
 def box_row_reduction(sudoku):
-    sudoku2 = row_remover(sudoku)
+
     for row in range(9):
         for cols in [0, 3, 6]:
-            for num in range(1,10):
-                sudoku2 = row_pots_list(num, row, cols, sudoku2)
+            for num in range(1, 10):
+                sudoku2 = row_pots_list(num, row, cols, sudoku)
     return sudoku2
 
+
 def box_cols_reduction(sudoku):
-    sudoku2 = column_remover(sudoku)
+    
     for cols in range(9):
         for row in [0, 3, 6]:
-            for num in range(1,10):
-
-                sudoku2 = col_pots_list(num, row, cols, sudoku2)
+            for num in range(1, 10):
+                sudoku2 = col_pots_list(num, row, cols, sudoku)
 
     return sudoku2
 
 def box_line_reduction(sudoku):
     sudoku2 = box_row_reduction(sudoku)
     sudoku3 = box_cols_reduction(sudoku2)
-    
+
     puzzle_reduced = list_to_int(sudoku3)
-    
+    print('Box_Line_Reduction')
+
     return puzzle_reduced
 
-def intersection(puzzle):
+# INTERSECTION CODE END
+
+# ADD IN EVERYONE ELSE'S FUNCTIONS HERE
+###
+
+
+
+
+# MAIN LOOP CODE
+
+# THESE ARE THE LOOPS CALLING YOUR FUNCTIONS CHECK FUNCTIONS FINISH WITH A LIST TO INTEGER/INCLUDE IN HERE
+def main_loop(puzzle):
+    puzzle_copy = copy.deepcopy(puzzle)
+    puzzle = simple_elim(puzzle)
+    if not puzzle_copy == puzzle:
+        main_loop(puzzle)
+    else:
+        puzzle_copy = copy.deepcopy(puzzle)
+        puzzle = box_line_reduction(puzzle)
+        if not puzzle_copy == puzzle:
+            main_loop(puzzle)
+        else:
+            puzzle_copy = copy.deepcopy(puzzle)
+            puzzle = pointing_reduction(puzzle)
+            if not puzzle_copy == puzzle:
+                main_loop(puzzle)
+
+                
+    return puzzle
+
+
+# CHECK IF SOLVED
+def is_solved(puzzle):
+    for i in range(9):
+        for j in range(9):
+            if is_list(puzzle[i][j]):
+                return False
+    return True
+
+
+# IF NOT SOLVED RUN THIS TO RETURN THE LISTS AS 0'z
+def failed_sudoku(puzzle):
+    for i in range(9):
+        for j in range(9):
+            if is_list(puzzle[i][j]):
+                puzzle[i][j] = 0
+    return puzzle
+
+
+# FINAL FUNCTION
+def sudoku_solver(puzzle):
+    puzzle = convert_to_listed(puzzle)
+    puzzle = main_loop(puzzle)
+    return puzzle
+
+# MAIN LOOP CODE END
+
+
+# TESTING CODE
+
+# This code is purely for testing purposes and is not used at all in the final
+# product.
+
+# When using the testing code please comment out the UI code before using.
+
+# When not using the testing code please comment out like it is now.
+
+'''
+
+def test_loop(puzzle):
     
     same = False
-    
-    initial_step(puzzle)
     
     while same == False:
         
         puzzle_copy = copy.deepcopy(puzzle)
+        print(puzzle)
+        puzzle1 = convert_to_listed(puzzle)
+        # INSERT THE FUNCTION YOU WANT TO TEST LOOP BELOW
+        puzzle2 = simple_elim(puzzle1)
+        puzzle3 = pointing_reduction(puzzle2)
+        puzzle4 = box_line_reduction(puzzle3)
+        puzzle5 = hidden_singles_elimination(puzzle4)
+        puzzle6 = naked_pairs_elimination(puzzle5)
         
-        initial_step(puzzle)
-        pointing_reduction(puzzle)
-        box_line_reduction(puzzle)
-
-        if puzzle_copy == puzzle:
+        if puzzle6 == puzzle_copy:
             same = True
             
     return puzzle
 
+# This is a solvable sudoku that can be used for testing purposes
+sudoku =[[4, 0, 0, 0, 0, 0, 9, 3, 8],
+          [0, 3, 2, 0, 9, 4, 1, 0, 0],
+          [0, 9, 5, 3, 0, 0, 2, 4, 0],
+          [3, 7, 0, 6, 0, 9, 0, 0, 4],
+          [5, 2, 9, 0, 0, 1, 6, 7, 3],
+          [6, 0, 4, 7, 0, 3, 0, 9, 0],
+          [9, 5, 7, 0, 0, 8, 3, 0, 0],
+          [0, 0, 3, 9, 0, 0, 4, 0, 0],
+          [2, 4, 0, 0, 3, 0, 7, 0, 9]]
+
+print('This is your inputted sudoku: ')
+for i in range(9):
+    print(sudoku[i])
     
+puzzle = convert_to_listed(sudoku)
+puzzle_complete = test_loop(puzzle)
+
+print("Here is your solved puzzle: ")
+for i in range(9):
+    print(puzzle_complete[i])
+
+# TESTING CODE END
+
+'''
+
+# USER INTERFACE CODE
+
+print("Enter your sudoku puzzle row by row, with a space between entries. (Write 0 for an empty cell). ")
+sudoku = []
+for i in range(9):
+    istr = str(i + 1)
+    b = input('Enter row number ' + istr + ' : ' )
+    numbers = b.split(' ')
+    row = [int(i) for i in numbers]
+    sudoku.append(row)
+
+print('This is your inputted sudoku: ')
+for i in range(9):
+    print(sudoku[i])
+puzzle1 = sudoku_solver(sudoku)
+if is_solved(puzzle1):
+    print("Here is your solved puzzle: ")
+    for i in range(9):
+        print(puzzle1[i])
+else:
+    print("I was unable to solve your puzzle. This is as far as I could solve it: ")
+    for i in range(9):
+        print(puzzle1[i])
+    print("This is as far as i could solve it, replacing all lists with 0's")
+    puzzle2 = failed_sudoku(puzzle1)
+    for i in range(9):
+        print(puzzle2[i])
+
+# USER INTERFACE CODE END
+
+
+
+
+
+
