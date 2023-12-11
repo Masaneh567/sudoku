7afa732c26e59120cb8971a82a023025da21656c
Jamesb383 <149089820+Jamesb383@users.noreply.github.com>
2023-12-11 19:07:50 +0000

    X-wing.py

--- X-wing.py
+++ X-wing.py
@@ -11,38 +11,6 @@ puzzle = [[1,[3,7,8],[3,7],[2,3,4,7,8],[2,7,8],[2,3,4,7,8],5,6,9],
           [[3,7,8],4,[3,7],5,[2,7,8,9],[2,3,7,8],[3,7,9],1,6],
           [9,[3,7,8],5,[3,7,8],6,1,4,[3,7,8],2],
           [6,2,1,[3,4,7,8],[7,8,9],[3,4,7,8],[3,7,9],[3,7,8,9],5]]
-          
-def convert_to_listed(sudoku):
-    for row in sudoku:
-        for col in range(9):
-            if row[col] == 0:
-                row[col] = list(range(1,10))
-    return sudoku
-
-def is_list(x):
-   if type(x) is list:
-       return True
-   else:
-       return False          
-            
-def list_to_int(sudoku):
-    for row in sudoku:
-        for col in range(9):
-            if is_list(row[col]) and len(row[col]) == 1:
-                row[col] = row[col][0]
-    return sudoku
-    
-def column_to_row(list_of_lists):
-    unlisted = []
-    # a list of all the entries in row order unlisted
-    for row in range(len(list_of_lists)): 
-        for col in range(len(list_of_lists)):
-                unlisted.append(list_of_lists[col][row])
-    relisted = []
-    # putting entries into 9 lists of 9
-    for i in range(0, len(unlisted), 9):
-        relisted.append(unlisted[i:i+9])
-    return relisted
 
 def find_columns(row, n):
     # returns list of columns of which n is a candidate in a given row
@@ -115,6 +83,38 @@ def row_x_wing(sudoku):
                     if n in row[c2]:
                         row[c2].remove(n)
     return sudoku
+
+def convert_to_listed(sudoku):
+    for row in sudoku:
+        for col in range(9):
+            if row[col] == 0:
+                row[col] = list(range(1,10))
+    return sudoku
+
+def is_list(x):
+   if type(x) is list:
+       return True
+   else:
+       return False          
+            
+def list_to_int(sudoku):
+    for row in sudoku:
+        for col in range(9):
+            if is_list(row[col]) and len(row[col]) == 1:
+                row[col] = row[col][0]
+    return sudoku
+    
+def column_to_row(list_of_lists):
+    unlisted = []
+    # a list of all the entries in row order unlisted
+    for row in range(len(list_of_lists)): 
+        for col in range(len(list_of_lists)):
+                unlisted.append(list_of_lists[col][row])
+    relisted = []
+    # putting entries into 9 lists of 9
+    for i in range(0, len(unlisted), 9):
+        relisted.append(unlisted[i:i+9])
+    return relisted
                                                                                                           
 def col_x_wing(sudoku):
     # same as row x-wing except with columns and rows switched
